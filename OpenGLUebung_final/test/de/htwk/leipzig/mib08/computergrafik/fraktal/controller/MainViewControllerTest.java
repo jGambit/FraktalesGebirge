4313d088126b7aea7ca390d7f93bd264d26a86c1
Enrico Timoschenko <enrico.timoschenko@web.de>
2013-11-24 10:19:50 +0100

    Unit-Test (TDD)

---
+++ OpenGLUebung_final/test/de/htwk/leipzig/mib08/computergrafik/fraktal/controller/MainViewControllerTest.java
@@ -0,0 +1,46 @@
+package de.htwk.leipzig.mib08.computergrafik.fraktal.controller;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+
+import javax.swing.JMenuItem;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import de.htwk.leipzig.mib08.computergrafik.fraktal.gui.MyPanel;
+
+public class MainViewControllerTest {
+	
+	private MainFrame mainFrame;
+	@Mock private JMenuItem mockItem;
+	@Mock private MyPanel myPanel;
+	
+	@Before
+	public void before() {
+		MockitoAnnotations.initMocks(this);
+		mainFrame = new MainFrame();
+	}
+	
+	@Test
+	public void testVisible() {
+		assertTrue(mainFrame.isVisible());
+		
+		mainFrame.setVisible(false);
+		
+		assertFalse(mainFrame.isVisible());
+	}
+	
+	@Test
+	public void testMenu() {
+		mainFrame.gp = myPanel;
+		
+		mainFrame.Neu.doClick();
+		
+		verify(myPanel).display();
+	}
+	
+}
